2243b856504165e9f8121fb0065dc4f91d8876bf
guronanak <guronanak@anderhost.de>
2022-04-16 17:15:08 +0100

    refs #ae761632 : Commit current state which is fully functional
    Check if any fixes are required later.
    
    Consider enhancements as well

--- download.js
+++ download.js
@@ -5,14 +5,14 @@ browser.runtime.onMessage.addListener(request => {
 
 function checkAndDownload() {
     // If transcript is already open
-    if (document.querySelectorAll('.cue').length){
+    if (document.querySelectorAll('.segment').length){
         download();
     }
     else{
-        document.querySelector('ytd-menu-renderer.ytd-video-primary-info-renderer > yt-icon-button:nth-child(2) > button:nth-child(1)').click();
+        document.querySelector('.ytd-video-primary-info-renderer > #button > #button > .style-scope').click();
     
         setTimeout(() => {
-            document.querySelector('ytd-menu-service-item-renderer.style-scope:nth-child(2)').click();
+            document.querySelector('.ytd-menu-service-item-renderer:nth-child(2)').click();
         }, 2000);
     
         setTimeout(download, 10000);
@@ -22,9 +22,14 @@ function checkAndDownload() {
 
 function download() {
     var transcript = '';
-    document.querySelectorAll('.cue').forEach(function (item, index) {
-        transcript = transcript + ' ' + item.innerText;
-    })
+    navigator.clipboard.writeText("")
+    timestamps = document.querySelectorAll('.segment-timestamp');
+    texts = document.querySelectorAll('.segment-text');
+    linesCount = timestamps.length;
+    for (let i = 0; i < linesCount ; i++){
+        transcript = transcript + timestamps[i].innerText + ' - ' + texts[i].innerText + '\n';
+    }
     console.log(transcript);
     navigator.clipboard.writeText(transcript);
 }
+
